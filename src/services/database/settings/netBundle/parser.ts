a810ac890fb024d92927a3bbd7d03ee1d6a7c80b
Tomasz Kasperczyk <kontakt@tomaszkasperczyk.name>
2022-06-21 15:44:58 +0200

    Fixing Codacy false issues

--- src/services/database/settings/netBundle/parser.ts
+++ src/services/database/settings/netBundle/parser.ts
@@ -1,4 +1,6 @@
 import { MessageParser } from "@framework/helpers/communication/socket";
 
+//Test
+
 export class NetBundleMessageParser extends MessageParser { }
 export const netBundleMessageParser = new MessageParser("netBundle", true);
